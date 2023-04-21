cd5070fd0fc535f20a46a380a51ab522d2128f7c
Arear <119037037+Arear2004@users.noreply.github.com>
2023-04-21 13:44:07 +0300

    Add files via upload

---
+++ gd6/gd6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace gd6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input text");
+            string s = Console.ReadLine();
+            Console.WriteLine(Regex.Replace(s, "[ауоиюяеёыэ]", ""));
+        }
+    }
+}
